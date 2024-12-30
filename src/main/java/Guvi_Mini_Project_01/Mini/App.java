7b2d062c98d3d81e1529b54e1fd8715cc9280e19
Ganesh1199-GMS <sganesh1199@gmail.com>
2024-12-30 08:58:28 +0530

    mini project

---
+++ src/main/java/Guvi_Mini_Project_01/Mini/App.java
@@ -0,0 +1,13 @@
+package Guvi_Mini_Project_01.Mini;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
